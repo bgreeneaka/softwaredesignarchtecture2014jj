7b32bdc61e75bc56414de9df4223a542206968d3
Brian <bgreeneaka@gmail.com>
2014-10-24 10:52:15 +0100

    Start of Observer with packages

--- SoftwareDesign/src/GUIComponent.java
+++
@@ -1,7 +0,0 @@
-
-public interface GUIComponent {
-	public void addComponent();
-	public void removeComponent();
-	public GUIComponent get(int index);
-	public void create();
-}
---
+++ SoftwareDesign/src/Observer/OpenFile.java
@@ -0,0 +1,9 @@
+package Observer;
+
+public class OpenFile {
+
+	public OpenFile() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
