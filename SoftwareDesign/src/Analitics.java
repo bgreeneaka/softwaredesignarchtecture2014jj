430c5075bd08f52335ae9a6dbcad82b95b3b9906
Bart <bkaminiecki@gmail.com>
2014-10-24 14:47:00 +0100

    GUI parts ready

---
+++ SoftwareDesign/src/Analitics.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+
+	public class Analitics extends JPanel {
+
+		JPanel panel = new JPanel();
+
+		Analitics() {// constructor
+
+			JLabel lbl = new JLabel("Analitics");
+			setBorder(BorderFactory.createLineBorder(Color.black));
+			add(lbl);
+
+		}
+	}
+
+
--- SoftwareDesign/src/GUI.java
+++ SoftwareDesign/src/GUI.java
@@ -1,49 +1,25 @@
-	import java.awt.*;
-	import java.awt.event.*;
-	import javax.swing.*;  //notice javax
-	public class GUI extends JFrame
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*; //notice javax
+
+public class GUI extends JFrame {
+	JPanel panel = new JPanel();
+
+	GUI() // constructor
+
 	{
-	 JPanel panel = new JPanel();
-	
-	  
-	  GUI() // constructor 
-	   
-	  {
-	    super("Text Mining"); 
-	    setBounds(100,100,800,600);
-	    add(userInput(),BorderLayout.NORTH);
-	    add(analitics(), BorderLayout.EAST);
-	    add(display1(),BorderLayout.WEST);
-	    add(display2(),BorderLayout.SOUTH);
-	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	  
-	    setVisible(true); // display frame
-	  }
-	
-	  private JPanel userInput(){
-		  JPanel panel = new JPanel();
-		  JLabel lbl = new JLabel("userInput");
-		  panel.add(lbl);
-		 return panel;
-	  }
-	  private JPanel analitics(){
-		  JPanel panel = new JPanel();
-		  JLabel lbl = new JLabel("analitics");
-		  panel.add(lbl);
-		  return panel;
-	  }
-	  private JPanel display1(){
-		  JPanel panel = new JPanel();
-		  JLabel lbl = new JLabel("display1");
-		  panel.add(lbl);
-		  return panel;
-	  }
-		 
-	  private JPanel display2(){
-		  JPanel panel = new JPanel();
-		  JLabel lbl = new JLabel("display2");
-		  panel.add(lbl);
-		  return panel;
-	  }
-	  
+		super("Text Mining");
+		setLayout(new GridLayout(2, 2));
+		setBounds(100, 100, 800, 600);
+		add(new UserInput());
+		add(new Analitics());
+		add(new DisplayText());
+		add(new DisplayGraphics());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		setVisible(true); // display frame
+	}
+
+
+
 }
