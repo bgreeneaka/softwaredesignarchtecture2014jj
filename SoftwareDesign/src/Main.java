5a1b848d46e157b72d4ffadd81f42898f5671e9a
Brian <bgreeneaka@gmail.com>
2014-10-24 18:50:51 +0100

    Test with 2 remotes

--- SoftwareDesign/src/Main.java
+++ SoftwareDesign/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 		// TODO Auto-generated constructor stub
 
 	public static void main(String[] args) {
-		System.out.println("test  for master");
+		System.out.println("test for master with 2 remotes");
 
 	}
 
