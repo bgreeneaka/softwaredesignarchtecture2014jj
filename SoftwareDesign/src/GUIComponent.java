9217584fe42678190484440e7e44326c877e306b
Brian <bgreeneaka@gmail.com>
2014-10-24 09:16:32 +0100

    Just started the GUI Process using the Composite Pattern

---
+++ SoftwareDesign/src/GUIComponent.java
@@ -0,0 +1,7 @@
+
+public interface GUIComponent {
+	public void addComponent();
+	public void removeComponent();
+	public GUIComponent get(int index);
+	public void create();
+}
---
+++ SoftwareDesign/src/SingleGUIElement.java
@@ -0,0 +1,32 @@
+
+public class SingleGUIElement implements GUIComponent {
+
+	public SingleGUIElement() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addComponent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeComponent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public GUIComponent get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
