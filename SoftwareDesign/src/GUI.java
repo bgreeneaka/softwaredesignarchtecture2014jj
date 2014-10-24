ff0543b259f28e0d02f921dce0355a8d568593b8
Bart <bkaminiecki@gmail.com>
2014-10-24 14:10:39 +0100

    testing3

--- SoftwareDesign/src/GUI.java
+++ SoftwareDesign/src/GUI.java
@@ -3,38 +3,20 @@
 	import javax.swing.*;  //notice javax
 	public class GUI extends JFrame
 	{
-	  JPanel panel = new JPanel(new GridLayout(2,2,2,2));
+	 JPanel panel = new JPanel();
+	
 	  
 	  GUI() // constructor 
 	   
 	  {
-	    super("Text Mining"); setBounds(100,100,800,600);
-	    //GridLayout GLayout = new GridLayout(2,2,2,2);
-		//panel.setLayout(GLayout);
+	    super("Text Mining"); 
+	    setBounds(100,100,800,600);
+	    add(userInput(),BorderLayout.NORTH);
+	    add(analitics(), BorderLayout.EAST);
+	    add(display1(),BorderLayout.WEST);
+	    add(display2(),BorderLayout.SOUTH);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	   // Container container = this.getContentPane();
-	    //container.add(userInput()); 
-	    //container.add(analitics()); 
-	    //container.add(display1()); 
-	   // 
-	   
-	    //container.add(display2()); 
-	   // panel.add(userInput()); 
-	   // panel.add(analitics()); 
-	   // panel.add(new JLabel("Enter Roll"));
-	   // panel.add(new JLabel("Enter Roll"));
-	   // panel.add(new JLabel("Enter Roll"));
-	   // panel.add(new JLabel("Enter Roll"));
-	    //panel.add(display1()); 
-	    //panel.add(display2()); 
-	    GridLayout experimentLayout = new GridLayout(0,2);
-	    panel.setLayout(experimentLayout);
-
-	    panel.add(new JButton("Button 1"));
-	    panel.add(new JButton("Button 2"));
-	    panel.add(new JButton("Button 3"));
-	    panel.add(new JButton("Long-Named Button 4"));
-	    panel.add(new JButton("5"));
+	  
 	    setVisible(true); // display frame
 	  }
 	
