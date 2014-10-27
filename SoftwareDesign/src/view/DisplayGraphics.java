2ca5db4a7db8731de500a182ed0e160d7e2d9ad1
Bart <bkaminiecki@gmail.com>
2014-10-27 11:38:29 +0000

    Test before i  push something conflicting, Test if i can push from Home PC.

---
+++ SoftwareDesign/src/view/DisplayGraphics.java
@@ -0,0 +1,19 @@
+package view;
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class DisplayGraphics extends JPanel {
+
+	JPanel panel = new JPanel();
+
+	public DisplayGraphics() {// constructor
+
+		JLabel lbl = new JLabel("DisplayGraphics");
+		setBorder(BorderFactory.createLineBorder(Color.black));
+		add(lbl);
+
+	}
+}
